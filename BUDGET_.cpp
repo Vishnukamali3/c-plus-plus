0c56673b625ae97486e5e184f195c7b959532cb2
Vishnukamali3 <166210006+Vishnukamali3@users.noreply.github.com>
2024-10-17 19:09:03 +0530

    Add files via upload

---
+++ BUDGET_.cpp
@@ -0,0 +1,21 @@
+#include <stdc++.h>
+#include<bits.h>
+using namespace std;
+
+int main() 
+{
+	int t,x,y;
+	cin>>t;
+	while(t--)
+	{
+	    cin>>x>>y;
+	    if(y*30<=x)
+	    {
+	        cout<<"yes\n";
+	    }
+	    else
+	    {
+	        cout<<"no\n";
+	    }
+	}
+}
